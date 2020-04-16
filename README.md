935ca9aa3c8e27d90d8bc7f35ae68853e7672293
seotaro <mail2017@seotaro.com>
2020-04-17 01:36:49 +0900

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# radio
+# ネットラジオ シェルスクリプト
+
+## 「らじる★らじる」シェルスクリプト
+
+usage: ./rec-nhk-radio.sh {NHK1|NHK2|FM} {長さ(分)} {番組名} {番組パーソナリティ} {出力ディレクトリ}
+
+NHKラジオのストリーミングサービス「らじる★らじる」を.m4a形式で指定した出力ディレクトリに保存する。ファイル名称は"*番組名* (*保存開始時刻(YYYYMMDD_hhmmss)*).m4a"。
+
+下記文字列を対応するタグに埋め込む。
+
+| 文字列                     | タグ         |
+| -------------------------- | ------------ |
+| ラジオ                     | genre        |
+| *番組パーソナリティ*       | album_artist |
+| *番組名* *YYYY/MM/DD* 放送 | title        |
+| *番組名*                   | album        |
+| *保存開始時刻(YYYY/MM/DD)* | date         |
+
+### 実行例
+
+```bash
+$ ./rec-nhk-radio.sh FM  120 "ジャズ・トゥナイト" "大友 良英" "/mnt/Music/ラジオ/NHK FM/ジャズ・トゥナイト"
+$ ls "/mnt/Music/ラジオ/NHK FM/ジャズ・トゥナイト"
+'ジャズ・トゥナイト (20200411_230001).m4a'
+```
+
+### cron 例
+
+```bash
+30 19 * * 1-5 /home/pi/rec-nhk-radio.sh FM  100 "ベストオブクラシック" "" "/mnt/Music/ラジオ/NHK FM/ベストオブクラシック"
+ 0 18 * * 6   /home/pi/rec-nhk-radio.sh FM   50 "ザ・ソウルミュージックII" "村上 てつや" "/mnt/Music/ラジオ/NHK FM/ザ・ソウルミュージックII"
+20 19 * * 6   /home/pi/rec-nhk-radio.sh FM  100 "N響 ザ・レジェンド" "檀 ふみ / 池辺 晋一郎" "/mnt/Music/ラジオ/NHK FM/N響 ザ・レジェンド"
+ 0 23 * * 6   /home/pi/rec-nhk-radio.sh FM  120 "ジャズ・トゥナイト" "大友 良英" "/mnt/Music/ラジオ/NHK FM/ジャズ・トゥナイト"
+30  8 * * 1-5 /home/pi/rec-nhk-radio.sh NHK2 10 "英会話タイムトライアル" "スティーブ・ソレイシィ" "/mnt/Music/ラジオ/NHK NHK2/英会話タイムトライアル"
+ 0  7 * * 6   /home/pi/rec-nhk-radio.sh NHK2 50 "英会話タイムトライアル" "スティーブ・ソレイシィ" "/mnt/Music/ラジオ/NHK NHK2/英会話タイムトライアル"
+```
---
+++ rec-nhk-radio.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# 変数とコマンド・予約語を区別したいので変数名は大文字にしている。
+
+NOW=`date '+%s'`
+
+if [ $# -ne 5 ]; then
+  echo "usage: $0 {NHK1|NHK2|FM} {長さ(分)} {番組名} {番組パーソナリティ} {出力ディレクトリ}"
+  exit 1
+fi
+
+# ハンドリングしやすいように引数を変数にマップする。
+CHANNEL=$1
+DURATION=`expr $2 \* 60`
+PROGRAM_NAME=$3
+PERSONALITY=$4
+DIRECTORY=$5
+
+# 作業用ディレクトリを作成して、一時ファイルはここに出力する。
+TMP="/tmp/rec-nhk-radio"
+mkdir -p ${TMP}
+
+
+# チャンネルのURLをセットする。
+# http://www.nhk.or.jp/radio/config/config_web.xml を参照のこと。
+M3U8URL=""
+case ${CHANNEL} in
+    "NHK1")
+      M3U8URL="https://nhkradioakr1-i.akamaihd.net/hls/live/511633/1-r1/1-r1-01.m3u8"
+      ;;
+    
+    "NHK2")
+      M3U8URL="https://nhkradioakr2-i.akamaihd.net/hls/live/511929/1-r2/1-r2-01.m3u8"
+      ;;
+
+    "FM")
+      M3U8URL="https://nhkradioakfm-i.akamaihd.net/hls/live/512290/1-fm/1-fm-01.m3u8"
+      ;;
+
+    *)
+      echo "failed channel"
+      exit 1
+      ;;
+esac
+ 
+
+# ハンドリングしやすいようにffmpegで書き込むメタデーターを別の変数にマップする。
+METADATA_GENRE="ラジオ"
+METADATA_ALBUM_ARTIST=${PERSONALITY}
+METADATA_TITLE="${PROGRAM_NAME} `date --date=@${NOW} '+%Y/%m/%d'` 放送"
+METADATA_ALBUM=${PROGRAM_NAME}
+METADATA_DATE=`date --date=@${NOW} '+%Y/%m/%d %H:%M:%S'`
+
+FILENAME="${PROGRAM_NAME} (`date --date=@${NOW} '+%Y%m%d_%H%M%S'`)"
+
+ffmpeg -loglevel quiet \
+        -y -i ${M3U8URL} \
+        -t ${DURATION} \
+        -metadata genre="${METADATA_GENRE}" \
+        -metadata album_artist="${METADATA_ALBUM_ARTIST}" \
+        -metadata title="${METADATA_TITLE}" \
+        -metadata album="${METADATA_ALBUM}" \
+        -metadata date="${METADATA_DATE}" \
+        -c copy "${TMP}/${FILENAME}.m4a"
+
+
+mkdir -p "${DIRECTORY}"
+cp "${TMP}/${FILENAME}.m4a" "${DIRECTORY}/${FILENAME}.m4a"
+rm -f "${TMP}/${FILENAME}.m4a"
+
+exit 0
